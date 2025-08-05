faf70a17ec712fc8ccdfffbf51d1104383fe68a9
amayl <amayl6969@gmail.com>
2025-08-05 13:37:16 +0100

    installed monaco editor for react

--- algovision/src/App.tsx
+++ algovision/src/App.tsx
@@ -1,5 +1,21 @@
+import type React from "react";
+
 function App() {
-  return <h1>Hello world</h1>;
+  // css stored in variables for the elements
+  const titleStyle: React.CSSProperties = {
+    textAlign: "center",
+    fontFamily: "Helvetica, Arial, sans-serif",
+    fontWeight: "bold",
+    marginBottom: "1.5rem",
+    letterSpacing: "0.5px",
+  };
+
+  return (
+    <>
+      <h1 style={titleStyle}>AlgoVision</h1>
+      <h3 style={titleStyle}>Paste your algorithm here</h3>
+    </>
+  );
 }
 
 export default App;
