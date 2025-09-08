ea85c710503a0021f2dd7443987feaab60dd2714
amayl <amayl6969@gmail.com>
2025-09-08 01:09:35 +0100

    made minor amends for deployment

--- algovision/src/App.tsx
+++ algovision/src/App.tsx
@@ -102,7 +102,7 @@ function App() {
     }
 
     try {
-      const res = await fetch("http://localhost:5000/analyse", {
+      const res = await fetch("api/analyse", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
